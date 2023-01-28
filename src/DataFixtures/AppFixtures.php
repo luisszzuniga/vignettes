b18972cd3171a17cf1a6f06aecad257d56e50500
luisszzuniga <78761028+luisszzuniga@users.noreply.github.com>
2023-01-28 16:04:35 +0100

    FEATURE: Ajout de l'authentification

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class AppFixtures extends Fixture
+{
+    private $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        //Création d'un admin
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setPassword(
+            $this->passwordHasher->hashPassword(
+                $user,
+                'password'
+            )
+        );
+        $user->setRoles([User::ROLE_ADMIN]);
+        $user->setValidated(true);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
